3901b21c2bd1341c011fdeb20954deb43799425a
Santosh Bhandari <santoshvandari1@gmail.com>
2025-05-11 17:15:25 +0545

    GeeksForGeeks Day 42

---
+++ Day42/main.py
@@ -0,0 +1,40 @@
+#User function Template for python3
+class Solution:
+	def twoSum(self, arr, target):
+		# code here
+	    seen = set()
+	    for num in arr:
+	        complement = target - num
+	        
+	        if complement in seen:
+	            return True
+	        seen.add(num)
+	    return False
+	            
+	            
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+#Initial Template for Python 3
+
+
+def main():
+    T = int(input())
+    while T > 0:
+        x = int(input())
+        arr = list(map(int, input().strip().split()))
+        ob = Solution()
+        ans = ob.twoSum(arr, x)
+        if ans:
+            print("true")
+        else:
+            print("false")
+        T -= 1
+        print("~")
+
+
+if __name__ == "__main__":
+    main()
+
+# } Driver Code Ends
