f76dfb45549f1a41c84aaa0780c3fdfbbae428e0
Santosh Bhandari <santoshvandari1@gmail.com>
2025-05-16 11:13:53 +0545

    Day 47

---
+++ Day47/main.py
@@ -0,0 +1,50 @@
+ #User function Template for python3
+ 
+class Solution:
+    
+    # arr[] : the input array
+    
+    #Function to return length of longest subsequence of consecutive integers.
+    def longestConsecutive(self,arr):
+        #code here
+        st = set(arr)
+        result = 0
+        
+        for value in arr:
+            if value in st and (value - 1) not in st:
+                current = value
+                count = 0
+                while current in st:
+                    st.remove(current)
+                    current = current +1
+                    count = count +1
+                result = max(result,count)
+        return result
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+import atexit
+import io
+import sys
+
+_INPUT_LINES = sys.stdin.read().splitlines()
+input = iter(_INPUT_LINES).__next__
+_OUTPUT_BUFFER = io.StringIO()
+sys.stdout = _OUTPUT_BUFFER
+
+
+@atexit.register
+def write():
+    sys.__stdout__.write(_OUTPUT_BUFFER.getvalue())
+
+
+if __name__ == '__main__':
+    t = int(input())
+    for tt in range(t):
+        a = list(map(int, input().strip().split()))
+        print(Solution().longestConsecutive(a))
+        print("~")
+# } Driver Code Ends
